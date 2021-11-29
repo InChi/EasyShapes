6027b564d753e1e5dd08249cae5ff4e57cc4fe87
InChi <inchi.85@gmail.com>
2021-11-29 06:50:12 +0200

    first
    first

---
+++ Meshkov_test_v1.0/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.enlargeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decreaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(0, 0);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(74, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Rectangle";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(0, 23);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Circle";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enlargeToolStripMenuItem,
+            this.decreaseToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // enlargeToolStripMenuItem
+            // 
+            this.enlargeToolStripMenuItem.Name = "enlargeToolStripMenuItem";
+            this.enlargeToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.enlargeToolStripMenuItem.Text = "+ (increase size)";
+            this.enlargeToolStripMenuItem.Click += new System.EventHandler(this.enlargeToolStripMenuItem_Click);
+            // 
+            // decreaseToolStripMenuItem
+            // 
+            this.decreaseToolStripMenuItem.Name = "decreaseToolStripMenuItem";
+            this.decreaseToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.decreaseToolStripMenuItem.Text = "- (decrease size)";
+            this.decreaseToolStripMenuItem.Click += new System.EventHandler(this.decreaseToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.deleteToolStripMenuItem.Text = "delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(98, 48);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.saveToolStripMenuItem.Text = "save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.loadToolStripMenuItem.Text = "load";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Name = "Form1";
+            this.Text = "Easy Shapes";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem enlargeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decreaseToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+    }
+}
+
---
+++ Meshkov_test_v1.0/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics g;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+        // ideally create abstract class Shape and have children classes (circle, square, triangle, etc.) inherit from Shape class and override the Draw() method
+        public class Circle
+        {
+            public int Radius { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Circle(int mouseX, int mouseY, int iRadius)
+            {
+                X = mouseX;
+                Y = mouseY;
+                Radius = iRadius;
+            }
+        }
+
+        
+        List<Rectangle> rectangles = new List<Rectangle>();
+        List<Circle> circles = new List<Circle>();
+
+        
+        int orderNumShape; // variable to store which shape from the list is selected by mouseclick
+
+        // idea for now is that 0 means Rectangle and 1 means Circle (may be improved with enum?)
+        byte radioButtonShape; // which shape is chosen by the user
+        byte rightClickCheckForm; // which shape was right clicked
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            g = CreateGraphics();
+
+            
+            int squareSide = 40;    // ensure it may be devided by 2 - or improve the code to make the check for this
+            int circleRadius = 25;
+
+            //creating shapes here to use them in the intersection check algorythms (which need improvement - bugs with circle intersection)
+            Rectangle rectangleNew = new Rectangle(e.X - squareSide/2, e.Y - squareSide/2, squareSide, squareSide);
+            Circle circleNew = new Circle(e.X - circleRadius, e.Y - circleRadius, circleRadius);
+
+
+            
+            if (e.Button == MouseButtons.Left && radioButtonShape == 0)
+            {
+
+                var intersect = false;
+
+                foreach (var rect in rectangles)
+                {
+                    if (rectangleNew.IntersectsWith(rect))
+                    {
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (!intersect)
+                {
+                    foreach (var circle in circles)
+                    {
+                        var distance = Math.Sqrt((Math.Pow(circleNew.X - circle.X, 2) + Math.Pow(circleNew.Y - circle.Y, 2)));
+                        if (distance < (circleNew.Radius + circle.Radius))
+                        {
+                            intersect = true;
+                            break;
+                        }
+                    }
+                }
+
+
+                if (intersect || (e.X < squareSide / 2) || (e.X > this.Width - squareSide) || (e.Y < squareSide / 2) || (e.Y > this.Height - squareSide - 20))
+                {
+                    MessageBox.Show("Error: intersection detected.");
+                }
+                else
+                {
+                    g.DrawRectangle(Pens.Blue, rectangleNew);
+                    rectangles.Add(rectangleNew);
+                }
+            }
+
+
+
+
+
+            // this solution must be improved. some repeating code is present.
+            // also there are bugs with circle intersection check
+
+            if (e.Button == MouseButtons.Left && radioButtonShape == 1)
+            {
+
+                var intersect = false;
+
+                foreach (var circle in circles)
+                {
+                    var distance = Math.Sqrt((Math.Pow(circleNew.X - circle.X, 2) + Math.Pow(circleNew.Y - circle.Y, 2)));
+                    if (distance < (circleNew.Radius + circle.Radius))
+                    {
+                        intersect = true;
+                        break;
+                    }
+                }
+
+                if (!intersect)
+                {
+                    foreach (var rect in rectangles)
+                    {
+                        if (rectangleNew.IntersectsWith(rect))
+                        {
+                            intersect = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (intersect || (e.X < circleRadius) || (e.X > this.Width - circleRadius - 20) || (e.Y < circleRadius) || (e.Y > this.Height - circleRadius - 40))
+                {
+                    MessageBox.Show("Error: intersection detected.");
+                }
+                else
+                {
+                    g.DrawEllipse(Pens.Blue, circleNew.X, circleNew.Y, circleNew.Radius * 2, circleNew.Radius * 2);
+                    circles.Add(circleNew);
+                }
+            }
+
+            else if (e.Button == MouseButtons.Right)
+            {
+                var hitShape = false;
+
+                for (int i = 0; i < rectangles.Count; i++)
+                {
+                    if (e.X > rectangles[i].X && e.X < (rectangles[i].X + rectangles[i].Width)
+                        && e.Y > rectangles[i].Y && e.Y < (rectangles[i].Y + rectangles[i].Height))
+                    {
+                        hitShape = true;
+                        orderNumShape = i;
+                        rightClickCheckForm = 0;
+
+                        contextMenuStrip1.Show(this, new Point(e.X, e.Y));
+                    }
+                }
+
+                // check if Circle was hit only if Square is not hit
+                // though right now, due to enlargement there may be intersection and therefore multiple shapes may be hit
+                // either add intersection check for enlargement or improve this code
+                if (!hitShape)
+                {
+                    for (int i = 0; i < circles.Count; i++)
+                    {
+                        var distance = Math.Sqrt((Math.Pow(e.X - circles[i].Radius - circles[i].X, 2) + Math.Pow(e.Y - circles[i].Radius - circles[i].Y, 2)));
+                        if (distance < circles[i].Radius)
+                        {
+                            hitShape = true;
+                            orderNumShape = i;
+                            rightClickCheckForm = 1;
+
+                            contextMenuStrip1.Show(this, new Point(e.X, e.Y));
+                        }
+                    }
+                }
+
+
+                if (!hitShape)
+                {
+                    contextMenuStrip2.Show(this, new Point(e.X, e.Y));
+                }
+            }
+        }
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            // 0 means Rectangle (may be improved with enum?)
+            radioButtonShape = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            // 1 means Circle (may be improved with enum?)
+            radioButtonShape = 1;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            // check to ensure that shape does not exceed 100
+            if ((rightClickCheckForm == 0 && rectangles[orderNumShape].Height > 100)
+                || (rightClickCheckForm == 1 && circles[orderNumShape].Radius*2 > 100))
+            {
+                enlargeToolStripMenuItem.Enabled = false;
+            }
+            else
+            {
+                enlargeToolStripMenuItem.Enabled = true;
+            }
+
+            // check to ensure that shape can not be decreased into non-existence
+            if (rightClickCheckForm == 0 && rectangles[orderNumShape].Height <= 11
+                || (rightClickCheckForm == 1 && circles[orderNumShape].Radius <= 7))
+            {
+                decreaseToolStripMenuItem.Enabled = false;
+            }
+            else
+            {
+                decreaseToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rightClickCheckForm == 0)
+            {
+                g.DrawRectangle(Pens.LightGray, rectangles[orderNumShape]);
+
+                rectangles.Remove(rectangles[orderNumShape]);
+            }
+
+            else if (rightClickCheckForm == 1)
+            {
+                g.DrawEllipse(Pens.LightGray, circles[orderNumShape].X, circles[orderNumShape].Y, circles[orderNumShape].Radius*2, circles[orderNumShape].Radius*2);
+
+                circles.Remove(circles[orderNumShape]);
+            }
+
+        }
+
+        private void enlargeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // ideally should have a check for insersection here as well
+
+            if (rightClickCheckForm == 0)
+            {
+                g.DrawRectangle(Pens.LightGray, rectangles[orderNumShape]);
+
+                rectangles[orderNumShape] = new Rectangle(rectangles[orderNumShape].X, rectangles[orderNumShape].Y, rectangles[orderNumShape].Width + 11, rectangles[orderNumShape].Height + 11);
+
+                g.DrawRectangle(Pens.Blue, rectangles[orderNumShape]);
+            }
+
+            else if (rightClickCheckForm == 1)
+            {
+                g.DrawEllipse(Pens.LightGray, circles[orderNumShape].X, circles[orderNumShape].Y, circles[orderNumShape].Radius*2, circles[orderNumShape].Radius*2);
+
+                circles[orderNumShape] = new Circle(circles[orderNumShape].X - 7, circles[orderNumShape].Y - 7, circles[orderNumShape].Radius + 7);
+
+                g.DrawEllipse(Pens.Blue, circles[orderNumShape].X, circles[orderNumShape].Y, circles[orderNumShape].Radius*2, circles[orderNumShape].Radius*2);
+            }         
+        }
+
+        private void decreaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (rightClickCheckForm == 0)
+            {
+                g.DrawRectangle(Pens.LightGray, rectangles[orderNumShape]);
+
+                rectangles[orderNumShape] = new Rectangle(rectangles[orderNumShape].X, rectangles[orderNumShape].Y, rectangles[orderNumShape].Width - 11, rectangles[orderNumShape].Height - 11);
+
+                g.DrawRectangle(Pens.Blue, rectangles[orderNumShape]);
+            }
+
+            else if (rightClickCheckForm == 1)
+            {
+                g.DrawEllipse(Pens.LightGray, circles[orderNumShape].X, circles[orderNumShape].Y, circles[orderNumShape].Radius*2, circles[orderNumShape].Radius*2);
+
+                circles[orderNumShape] = new Circle(circles[orderNumShape].X + 7, circles[orderNumShape].Y + 7, circles[orderNumShape].Radius - 7);
+
+                g.DrawEllipse(Pens.Blue, circles[orderNumShape].X, circles[orderNumShape].Y, circles[orderNumShape].Radius*2, circles[orderNumShape].Radius*2);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // not implemented
+
+            
+            MessageBox.Show("not implemented");
+
+            //var result = string.Join("|", circles);
+            //System.IO.File.WriteAllText("SavedShapes.txt", result);
+
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // not implemented
+            MessageBox.Show("not implemented");
+        }
+    }
+
+}
